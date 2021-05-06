9f8f9192ca21aadb442249754c5ea2ac949a43d7
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-06 18:53:26 +0300

    hw-done

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import galleryItems from "./gallery-items.js";
 
-const toGaleryInsert = document.querySelector(".js-gallery");
+const galeryInsertReff = document.querySelector(".js-gallery");
+const modalWindowReff = document.querySelector(".js-lightbox");
+const lightboxImageReff = document.querySelector(".lightbox__image");
+const lightboxOverlayReff = document.querySelector(".lightbox__overlay");
+const buttonCloseModalReff = document.querySelector(".lightbox__button");
 
 const createHTML = createMarkup(galleryItems);
-
 function createMarkup(items) {
   const marcup = items
     .map(
@@ -22,8 +25,48 @@ function createMarkup(items) {
 </li>`
     )
     .join("");
-  
-  toGaleryInsert.insertAdjacentHTML("afterbegin", marcup);
 
-   return marcup;
+  galeryInsertReff.insertAdjacentHTML("afterbegin", marcup);
+
+  return marcup;
+}
+
+galeryInsertReff.addEventListener("click", modalWindowOpen);
+buttonCloseModalReff.addEventListener("click", modalWindowClose);
+window.addEventListener("keyup", modalWindowCloseBtn);
+modalWindowReff.addEventListener("click", lightboxOverlayClose);
+
+function atributChange(src = "", alt = "") {
+  lightboxImageReff.src = src;
+  lightboxImageReff.alt = alt;
+}
+function modalWindowOpen(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  modalWindowReff.classList.add("is-open");
+  atributChange(event.target.dataset.source, event.target.alt);
+  // lightboxImageReff.src = event.target.dataset.source;
+  // lightboxImageReff.alt = event.target.alt;
+}
+
+function modalWindowClose(event) {
+  modalWindowReff.classList.remove("is-open");
+  atributChange();
+  // lightboxImageReff.src = "";
+  // lightboxImageReff.alt = "";
+}
+function modalWindowCloseBtn(event) {
+  if (event.code === "Escape") {
+    modalWindowReff.classList.remove("is-open");
+  }
+  console.log(event.code);
+}
+
+function lightboxOverlayClose(event) {
+  if (event.target === lightboxOverlayReff) {
+    modalWindowReff.classList.remove("is-open");
+    atributChange();
+  }
 }

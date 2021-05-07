0ee22432720e7ebd8979976b7eb6ca8c0ecd1796
Serhiy Korytskyy <korysergey55@gmail.com>
2021-05-07 18:55:14 +0300

    slider-done

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const lightboxImageReff = document.querySelector(".lightbox__image");
 const lightboxOverlayReff = document.querySelector(".lightbox__overlay");
 const buttonCloseModalReff = document.querySelector(".lightbox__button");
 
+galeryInsertReff.addEventListener("click", modalWindowOpen);
+buttonCloseModalReff.addEventListener("click", modalWindowClose);
+window.addEventListener("keyup", modalWindowCloseBtn);
+modalWindowReff.addEventListener("click", lightboxOverlayClose);
+galeryInsertReff.addEventListener("keyup", slider);
+
+let indexImege = 0;
+
 const createHTML = createMarkup(galleryItems);
+
 function createMarkup(items) {
   const marcup = items
     .map(
-      (item) => `<li class="gallery__item">
+      (item, index) => `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${item.original}"
@@ -19,6 +28,7 @@ function createMarkup(items) {
       class="gallery__image"
       src="${item.preview}"
       data-source="${item.original}"
+      data-index='${index}'
       alt="${item.description}"
     />
   </a>
@@ -31,15 +41,11 @@ function createMarkup(items) {
   return marcup;
 }
 
-galeryInsertReff.addEventListener("click", modalWindowOpen);
-buttonCloseModalReff.addEventListener("click", modalWindowClose);
-window.addEventListener("keyup", modalWindowCloseBtn);
-modalWindowReff.addEventListener("click", lightboxOverlayClose);
-
 function atributChange(src = "", alt = "") {
   lightboxImageReff.src = src;
   lightboxImageReff.alt = alt;
 }
+
 function modalWindowOpen(event) {
   event.preventDefault();
   if (event.target === event.currentTarget) {
@@ -49,6 +55,9 @@ function modalWindowOpen(event) {
   atributChange(event.target.dataset.source, event.target.alt);
   // lightboxImageReff.src = event.target.dataset.source;
   // lightboxImageReff.alt = event.target.alt;
+
+  indexImege = Number(event.target.dataset.index);
+  console.log(indexImege);
 }
 
 function modalWindowClose(event) {
@@ -60,13 +69,28 @@ function modalWindowClose(event) {
 function modalWindowCloseBtn(event) {
   if (event.code === "Escape") {
     modalWindowReff.classList.remove("is-open");
+    atributChange();
   }
   console.log(event.code);
 }
-
 function lightboxOverlayClose(event) {
   if (event.target === lightboxOverlayReff) {
     modalWindowReff.classList.remove("is-open");
     atributChange();
   }
+}
+
+function slider(event) {
+  if (event.code === "ArrowRight") {
+    indexImege += 1;
+    if (indexImege > galleryItems.length -1) indexImege = 0;
+  }
+  if (event.code === "ArrowLeft") {
+    indexImege -= 1;
+    if (indexImege < 0) indexImege = galleryItems.length -1;
+  }
+  atributChange(
+    galleryItems[indexImege].original,
+    galleryItems[indexImege].description
+  );
 }
